5a0e546dd5577d48d87752a5a84343309994e951
zzq <chamrun_yf1@163.com>
2018-05-16 15:10:03 +0800

    modify eSenserStat to ipc_eSenserStat

--- inc/Ipcctl.h
+++ inc/Ipcctl.h
@@ -75,13 +75,13 @@ typedef enum
 }IPC_Class;
 
 typedef enum {
-	eSen_TV0	= 0x00,
-	eSen_TV1	= 0x01,
-	eSen_TV2   = 0x02,
-	eSen_TV3 	= 0x03,
-	eSen_FR1   = 0x04,
-	eSen_Max   = 0x05
-}eSenserStat;
+        ipc_eSen_TV0 = 0x00,
+        ipc_eSen_TV1 = 0x01,
+        ipc_eSen_TV2 = 0x02,
+        ipc_eSen_TV3 = 0x03,
+        ipc_eSen_FR1 = 0x04,
+        ipc_eSen_Max = 0x05
+}ipc_eSenserStat;
 
 typedef enum img_zoom
 {
