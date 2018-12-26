170ab66284a9ee70728661f55ab90eca866fa58f
zzq <zzq@qr.com>
2018-12-26 10:14:26 +0800

    新增OSD菜单相关变量

--- inc/Ipcctl.h
+++ inc/Ipcctl.h
@@ -84,6 +84,29 @@ typedef enum
 	shm_rdwr
 }shm_perms;
 
+typedef enum{
+	mainmenu0=0,
+	mainmenu1,
+	mainmenu2,
+	submenu_carli,
+	submenu_gunball,
+	submenu_mtd,
+	submenu_setimg,
+	submenu_setball,
+	submenu_setcom,
+	submenu_setnet,
+	menumaxid
+}AppMenuId;
+
+typedef struct
+{
+	int id;
+	int pointer;
+	int submenu_cnt;
+	int start;
+	int end;
+}AppMenu;
+
 typedef struct{
 	unsigned int panPos;
 	unsigned int tilPos;
@@ -575,6 +598,10 @@ typedef struct
 	
 	volatile unsigned int  	ImgMmtshow[ipc_eSen_Max];	//not sure show what
 	volatile unsigned char 	MmtOffsetXY[20]; 		//not sure the func
+
+	volatile unsigned int MenuStat;
+	AppMenu menuarray[menumaxid];
+	char Passwd[128];
 	
 } IMGSTATUS;
 
