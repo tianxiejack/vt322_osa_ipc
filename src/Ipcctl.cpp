178d5b48f04a775de4564250b26af398974afc46
alex <alex@qr.com>
2019-03-14 13:18:03 +0800

    更改结构

--- src/Ipcctl.cpp
+++ src/Ipcctl.cpp
@@ -4,50 +4,10 @@
 #include <sys/syscall.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include"Ipcctl.h"
-IPC_Handl Ipc_Handl[IPC_MAX];
+#include "ipc_custom_head.hpp"
+#include <vector>
 
-IMGSTATUS * IMGstatus=NULL;
-OSDSTATUS *OSDstatus=NULL;
-UTCTRKSTATUS *UTCTRKstatus=NULL;
-LKOSDSTATUS *LKOSDstatus = NULL;
-osdtext_t *OSDTEXTstatus;
-
-void Ipc_init()
-{
-	Ipc_Handl[IPC_TOIMG_MSG].name=_PATH0_;
-	Ipc_Handl[IPC_TOIMG_MSG].Identify=IPC_TOIMG_MSG;
-	Ipc_Handl[IPC_TOIMG_MSG].Class=IPC_Class_MSG;
-	
-	Ipc_Handl[IPC_FRIMG_MSG].name=_PATH1_;
-	Ipc_Handl[IPC_FRIMG_MSG].Identify=IPC_FRIMG_MSG;
-	Ipc_Handl[IPC_FRIMG_MSG].Class=IPC_Class_MSG;
-	
-	Ipc_Handl[IPC_SHA].name=_PATH2_;
-	Ipc_Handl[IPC_SHA].Identify=IPC_SHA;
-	Ipc_Handl[IPC_SHA].Class=IPC_Class_SHA;
-
-	Ipc_Handl[IPC_OSD_SHA].name=_PATH3_;
-	Ipc_Handl[IPC_OSD_SHA].Identify=IPC_OSD_SHA;
-	Ipc_Handl[IPC_OSD_SHA].Class=IPC_Class_SHA;
-
-	Ipc_Handl[IPC_UTCTRK_SHA].name=_PATH4_;
-	Ipc_Handl[IPC_UTCTRK_SHA].Identify=IPC_UTCTRK_SHA;
-	Ipc_Handl[IPC_UTCTRK_SHA].Class=IPC_Class_SHA;
-
-	Ipc_Handl[IPC_LKOSD_SHA].name=_PATH5_;
-	Ipc_Handl[IPC_LKOSD_SHA].Identify=IPC_LKOSD_SHA;
-	Ipc_Handl[IPC_LKOSD_SHA].Class=IPC_Class_SHA;
-
-	Ipc_Handl[IPC_OSDTEXT_SHA].name=_PATH6_;
-	Ipc_Handl[IPC_OSDTEXT_SHA].Identify=IPC_OSDTEXT_SHA;
-	Ipc_Handl[IPC_OSDTEXT_SHA].Class=IPC_Class_SHA;
-	
-	Ipc_Handl[IPC_SEM].name=_PATH7_;
-	Ipc_Handl[IPC_SEM].Identify=IPC_SEM;
-	Ipc_Handl[IPC_SEM].Class=IPC_Class_SEM;
-
-}
+std::vector <IPC_Handl_t> IpcHandl;
 
 int ipc_sendmsg(SENDST* Param,int Mesgthe )
 {
@@ -56,15 +16,11 @@ int ipc_sendmsg(SENDST* Param,int Mesgthe )
 	char buffer[MESSAGE_SIZE];
 	memset(buffer,0,sizeof(buffer));
 	int mtype=1;
-	//buffer[0]=Param->param1;
-	//buffer[1]=Param->param2;
     memcpy(buffer,Param,sizeof(SENDST));
 	setMessage(&msg, buffer, MESSAGE_SIZE, mtype);
 	/* Send message: */
-	ret = messageQueueSend(Ipc_Handl[Mesgthe].IPCID, &msg);
+	ret = messageQueueSend(IpcHandl[Mesgthe].IPCID, &msg);
 	return ret;
-
-
 }
 
 int ipc_recvmsg(SENDST* Param,int Mesgthe )
@@ -73,200 +29,71 @@ int ipc_recvmsg(SENDST* Param,int Mesgthe )
 	Message msg;
 	int mtype=1;
 	clearMessage(&msg);
-	ret = messageQueueReceive(Ipc_Handl[Mesgthe].IPCID, &msg, mtype);
-    	memcpy(Param,msg.buffer,sizeof(SENDST));
-
+	ret = messageQueueReceive(IpcHandl[Mesgthe].IPCID, &msg, mtype);
+    memcpy(Param,msg.buffer,sizeof(SENDST));
 	return ret;
-
-
-
 }
 void  ipc_status_P()
 {
-	semWait(Ipc_Handl[IPC_SEM].IPCID,0);
+	semWait(IpcHandl[IPC_SEM].IPCID,0);
 }
 void  ipc_status_V()
 {
-	semSignal(Ipc_Handl[IPC_SEM].IPCID,0);
+	semSignal(IpcHandl[IPC_SEM].IPCID,0);
 }
 
-
-
-int ipc_settrack(unsigned int trackstatus, float trackposx, float trackposy)
+void Ipc_create()
 {
-	int ret=0;
-	if(IMGstatus==NULL)
-		return -1;
-//	ret = sharedMemoryLock(Ipc_Handl[IPC_SHA].IPCID);
-	ipc_status_P();
-	IMGstatus->TrkStat=trackstatus;
-	IMGstatus->trkerrx=trackposx;
-	IMGstatus->trkerry=trackposy;
-	ipc_status_V();
-//	ret = sharedMemoryUnlock(Ipc_Handl[IPC_SHA].IPCID);
-
-	return ret;
-
-}
-
-int ipc_setSensorstat(unsigned int unitFaultStat)
-{
-	int ret=0;
-	if(IMGstatus==NULL)
-		return -1;
-	ipc_status_P();
-	IMGstatus->unitFaultStat=unitFaultStat;
-
-	ipc_status_V();
-
-	return ret;
-
-}
-
-
-int ipc_gettrack(unsigned int* trackstatus, float* trackposx, float* trackposy)
-{
-	int ret=0;
-	if(IMGstatus==NULL)
-		return -1;
-	if(trackstatus==NULL||trackposx==NULL||trackposy==NULL)
-		return -1;
-	ipc_status_P();
-
-	//ret = sharedMemoryLock(Ipc_Handl[IPC_SHA].IPCID);
-	*trackstatus=IMGstatus->TrkStat;
-	*trackposx=IMGstatus->trkerrx;
-	*trackposy=IMGstatus->trkerry;
-	ipc_status_V();
-//	ret = sharedMemoryUnlock(Ipc_Handl[IPC_SHA].IPCID);
-
-	return ret;
-
-}
-
-int ipc_getSensorstat(unsigned int* unitFaultStat)
-{
-	int ret=0;
-	if(IMGstatus==NULL)
-		return -1;
-	if(unitFaultStat==NULL)
-		return -1;
-	
-	ipc_status_P();
-
-	*unitFaultStat=IMGstatus->unitFaultStat;
-
-	ipc_status_V();
-
-	return ret;
-}
-
-IMGSTATUS *ipc_getimgstatus_p()
-{
-	return IMGstatus;
-}
-
-OSDSTATUS *ipc_getosdstatus_p()
-{
-	return OSDstatus;
-}
-
-UTCTRKSTATUS *ipc_getutstatus_p()
-{
-	return UTCTRKstatus;
-}
-LKOSDSTATUS *ipc_getlkosdstatus_p()
-{
-	return LKOSDstatus;
-}
-
-osdtext_t *ipc_getosdtextstatus_p()
-{
-	return OSDTEXTstatus;
-}
-
-IMGSTATUS ipc_getosdstatus()
-{
-	IMGSTATUS imgstatus;
-	ipc_status_P();
-	memcpy(&imgstatus, IMGstatus, sizeof(IMGSTATUS));
-	ipc_status_V();
-	return imgstatus;
-}
-
-void Ipc_create(int *shm_perm)
-{
-	for(int i=0;i<IPC_MAX;i++)
+	if(IpcHandl.size())
+	{
+		key_t key;
+		for(int i=0; i<IpcHandl.size(); i++)
 		{
-
-			key_t key;
-			if(Ipc_Handl[i].Class==IPC_Class_MSG)
-					{
-						
-						key=messageKeyGet(Ipc_Handl[i].name,Ipc_Handl[i].Identify);
-						Ipc_Handl[i].IPCID=messageQueueCreate(key);
-					}
-				else if(Ipc_Handl[i].Class==IPC_Class_SHA)
-					{
-							if(Ipc_Handl[i].Identify==IPC_SHA)
-									{
-											key=sharedKeyGet(Ipc_Handl[i].name,Ipc_Handl[i].Identify);
-											Ipc_Handl[i].IPCID=sharedMemoryCreateOrGet(key,sizeof(IMGSTATUS));
-											IMGstatus=(IMGSTATUS *)sharedMemoryAttach(Ipc_Handl[i].IPCID,shm_perm[Ipc_Handl[i].Identify]);
-									}
-							else if(Ipc_Handl[i].Identify==IPC_OSD_SHA)
-									{
-											key=sharedKeyGet(Ipc_Handl[i].name,Ipc_Handl[i].Identify);
-											Ipc_Handl[i].IPCID=sharedMemoryCreateOrGet(key,sizeof(OSDSTATUS));
-											OSDstatus=(OSDSTATUS *)sharedMemoryAttach(Ipc_Handl[i].IPCID,shm_perm[Ipc_Handl[i].Identify]);
-									}
-							else if(Ipc_Handl[i].Identify==IPC_UTCTRK_SHA)
-									{
-											key=sharedKeyGet(Ipc_Handl[i].name,Ipc_Handl[i].Identify);
-											Ipc_Handl[i].IPCID=sharedMemoryCreateOrGet(key,sizeof(UTCTRKSTATUS));
-											UTCTRKstatus=(UTCTRKSTATUS *)sharedMemoryAttach(Ipc_Handl[i].IPCID,shm_perm[Ipc_Handl[i].Identify]);
-									}
-							else if(Ipc_Handl[i].Identify==IPC_LKOSD_SHA)
-									{
-											key=sharedKeyGet(Ipc_Handl[i].name,Ipc_Handl[i].Identify);
-											Ipc_Handl[i].IPCID=sharedMemoryCreateOrGet(key,sizeof(LKOSDSTATUS));
-											LKOSDstatus=(LKOSDSTATUS *)sharedMemoryAttach(Ipc_Handl[i].IPCID,shm_perm[Ipc_Handl[i].Identify]);
-									}
-							else if(Ipc_Handl[i].Identify==IPC_OSDTEXT_SHA)
-									{
-											key=sharedKeyGet(Ipc_Handl[i].name,Ipc_Handl[i].Identify);
-											Ipc_Handl[i].IPCID=sharedMemoryCreateOrGet(key,sizeof(osdtext_t));
-											OSDTEXTstatus=(osdtext_t *)sharedMemoryAttach(Ipc_Handl[i].IPCID,shm_perm[Ipc_Handl[i].Identify]);
-									}
-					}
-				else if(Ipc_Handl[i].Class==IPC_Class_SEM)
-									{
-											if(Ipc_Handl[i].Identify==IPC_SEM)
-													{
-														key=semKeyGet(Ipc_Handl[i].name,Ipc_Handl[i].Identify);
-														Ipc_Handl[i].IPCID=semCreate(key,1);
-													}
-									}	
-		}
-}
-
-void Ipc_uninit()
-{
-	for(int i=0;i<IPC_MAX;i++)
-		{
-			if(Ipc_Handl[i].Class==IPC_Class_MSG)
-				{
-					messageQueueDelete(Ipc_Handl[i].IPCID);
-				}
-			else if(Ipc_Handl[i].Class==IPC_Class_SHA)
-				{
-					sharedMemoryDelete(Ipc_Handl[i].IPCID);
-				}
-			else if(Ipc_Handl[i].Class==IPC_Class_SEM)
+			switch(IpcHandl[i].Class)
 			{
-				semDelete(Ipc_Handl[i].IPCID);
+				case IPC_Class_MSG:
+					key = messageKeyGet(IpcHandl[i].name,IpcHandl[i].Identify);
+					IpcHandl[i].IPCID = messageQueueCreate(key);
+					break;
+				case IPC_Class_SHA:
+					key = sharedKeyGet(IpcHandl[i].name,IpcHandl[i].Identify);
+					IpcHandl[i].IPCID = sharedMemoryCreateOrGet(key,IpcHandl[i].length);
+					IpcHandl[i].ptr =(void *)sharedMemoryAttach(IpcHandl[i].IPCID,IpcHandl[i].RWmode);				
+					break;
+
+				case IPC_Class_SEM:
+					key=semKeyGet(IpcHandl[i].name,IpcHandl[i].Identify);
+					IpcHandl[i].IPCID=semCreate(key,1);
+					break;
+
+				default:
+					break;					
 			}
 		}
+	}
+	return ;
+}
+
+void Ipc_destory()
+{
+	if(IpcHandl.size())
+	{
+		for(int i=0;i<IpcHandl.size();i++)
+		{
+			if(IpcHandl[i].Class==IPC_Class_MSG)
+			{
+				messageQueueDelete(IpcHandl[i].IPCID);
+			}
+			else if(IpcHandl[i].Class==IPC_Class_SHA)
+			{
+				sharedMemoryDelete(IpcHandl[i].IPCID);
+			}
+			else if(IpcHandl[i].Class==IPC_Class_SEM)
+			{
+				semDelete(IpcHandl[i].IPCID);
+			}
+		}
+	}
 }
 
 
--- src/SharedMemoryWrapper.cpp
+++ src/SharedMemoryWrapper.cpp
@@ -1,69 +1,61 @@
 #include "SharedMemoryWrapper.h"
 #include <stdio.h>
- #include <string.h>
- #include "Ipcctl.h"
+#include <string.h>
+#include "ipc_custom_head.hpp"
 
 key_t sharedKeyGet(char *path,int proj_id)
 {
-
- key_t key = ftok(path,proj_id);
-    if(key < 0){
-        printf("%d:%s\n",errno,strerror(errno));
-        return -1;
-    }
-     return key;
+	key_t key = ftok(path,proj_id);
+	if(key < 0)
+	{
+		printf("%d:%s\n",errno,strerror(errno));
+		return -1;
+	}
+	return key;
 }
 int sharedMemoryGet(key_t key, int size)
 {
 	int shmid;
-	
 	if ((shmid = shmget(key, size, 0666)) == -1)
 		return -1;
-
 	return shmid;
 }
 
 int sharedMemoryCreate(key_t key, int size)
 {
 	int shmid;
-
 	if ((shmid = shmget(key, size, IPC_CREAT | 0666)) == -1)
 		return -1;
-
 	return shmid;
 }
 
 int sharedMemoryCreateOrGet(key_t key, int size)
 {
 	int shmid;
-
 	/* If memory has already been created.. then just get it: */
 
-		//if ((shmid = shmget(key, size, IPC_CREAT | IPC_EXCL | 0666)) == -1)
-		if ((shmid = shmget(key, size, IPC_CREAT | 0666)) == -1)
+	//if ((shmid = shmget(key, size, IPC_CREAT | IPC_EXCL | 0666)) == -1)
+	if ((shmid = shmget(key, size, IPC_CREAT | 0666)) == -1)
+	{
+		if( errno != EEXIST )
 		{
-			if( errno != EEXIST )
-			{
-				return -1;
-			}
-
-			if ((shmid = shmget(key, size, 0666)) == -1)
-			{
-				return -1;
-			}
+			return -1;
 		}
 
+		if ((shmid = shmget(key, size, 0666)) == -1)
+		{
+			return -1;
+		}
+	}
 	return shmid;
 }
 
 int sharedMemoryCreateIfGone(key_t key, int size)
 {
 	int shmid;
-
 	/* If memory has already been created.. then just get it: */
 	if ((shmid = shmget(key, size, IPC_CREAT | IPC_EXCL | 0666)) == -1)
 		return errno;
-
 	return shmid;
 }
 
@@ -79,7 +71,6 @@ int sharedMemoryDetatch(const void* shmaddr)
 {
 	if( shmdt(shmaddr) == -1 )
 		return errno;
-
 	return 0;
 }
 
@@ -87,7 +78,6 @@ int sharedMemoryDelete(int shmid)
 {
 	if( shmctl(shmid, IPC_RMID, 0) == -1 )
 		return errno;
-
 	return 0;
 }
 
@@ -103,6 +93,5 @@ int sharedMemoryUnlock(int shmid)
 {
 	if( shmctl(shmid, SHM_UNLOCK, 0) == -1 )
 		return errno;
-
 	return 0;
 }
